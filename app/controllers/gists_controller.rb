4326c466c92e9af62612302cfbe250825ab991ff
Stephanyan <rabena.stephane@gmail.com>
2017-09-14 17:59:52 +0200

    Add show new create edit update destroy methods in gist controller + gists_params and find_gist private methods + add before_action method

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,4 +1,37 @@
 class GistsController < ApplicationController
-  def index
+  before_action :find_gist, only: %i[show edit update destroy]
+
+  def index; end
+
+  def show; end
+
+  def new
+    @gist = Gist.new
+  end
+
+  def create
+    @gist = Gist.new(gists_params)
+
+    if @gist.save
+      redirect_to @gist
+    else
+      render 'New'
+    end
+  end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def gists_params
+    params.require(:gist).permit(:filename, :description, :code)
+  end
+
+  def find_gist
+    @gist = Gist.find(params[:id])
   end
 end
