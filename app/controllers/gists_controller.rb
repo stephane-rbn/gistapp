ef1a7b0fd4a72b09dadf8c5d68dcf1d667d30997
Stephanyan <rabena.stephane@gmail.com>
2017-09-16 15:09:44 +0200

    Add the ability to create and edit gists in GistController + add some comments

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,7 +1,10 @@
+# Defines gist actions
 class GistsController < ApplicationController
+  # Find gist by id before doing any of these actions
   before_action :find_gist, only: %i[show edit update destroy]
 
   def index
+    # Displays all gists in descending order
     @gists = Gist.all.order('created_at DESC')
   end
 
@@ -23,17 +26,28 @@ class GistsController < ApplicationController
 
   def edit; end
 
-  def update; end
+  def update
+    if @gist.update(gists_params)
+      redirect_to @gist
+    else
+      render 'Edit'
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @gist.destroy
+    redirect_to root_path
+  end
 
   private
 
   def gists_params
+    # Parameters we need to create and update a gist
     params.require(:gist).permit(:filename, :description, :code)
   end
 
   def find_gist
+    # Method that finds gist by id at the top of the show, edit, update and destroy methods
     @gist = Gist.find(params[:id])
   end
 end
