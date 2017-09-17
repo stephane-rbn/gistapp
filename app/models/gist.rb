72a0ee3ea93cffd19060fbc8dca38d7769ecd89e
Stephanyan <rabena.stephane@gmail.com>
2017-09-17 21:24:27 +0200

    Add search method with query in model: search by filename, code and description

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,4 +1,9 @@
 # Gist model
 class Gist < ApplicationRecord
   belongs_to :category
+
+  def self.search(search)
+    where('filename ILIKE ? OR code LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+    # TODO: refactor this long line
+  end
 end
